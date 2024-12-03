71551ae12c1af8b811fb6539ea3e4da0df9f9a8b
ebkaa <ebkaucsu@gmail.com>
2024-12-03 11:50:10 +0300

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -166,7 +166,7 @@ public class BaseTest {
                 capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
                 capabilities.setCapability("key", System.getenv("key"));
-                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hu"), capabilities);
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
             }
 
             //capabilities.setCapability("key", System.getenv("key"));
